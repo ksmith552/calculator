d03e4fa2900ec08397bea7cd5ef7222ab31da1ee
Kevin Smith <123609428+ksmith552@users.noreply.github.com>
2023-09-12 19:27:39 -0500

    Create function
    Created function to handle getting the values for num1 and num2 and
    removed obsolete code
    
    updated README

--- script.js
+++ script.js
@@ -39,65 +39,44 @@ function operate(a, operator, b) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-/*numberButtons.addEventListener("click", function(event) {
-    let display = document.getElementById("display-container")
-     let elementText = event.target.innerText
-    let num1 = elementText
-    if (elementText == "+") {
-        
-    }
-    console.log(num1)
-    //display.textContent = displayValue
-    //console.log(parseInt(displayValue))
-})
-*/
-/*numberButtons.addEventListener("click", function(event) {
-    let display = document.getElementById("display-container")
-     let elementText = event.target.innerText
-    let num2  = display.textContent + elementText
-    if (event.target.innerText == "=") {
-       return num2
-        //add(num1, num2)
-    }
-    
-    //display.textContent = displayValue
-    //console.log(parseInt(displayValue))
-})
-
-*/
-
 //Listens for display numbers
 
-let buttons = document.getElementById('calculator')
-buttons.addEventListener("click", function(event) {
+let buttons = document.getElementById('calculator')    
+buttons.addEventListener("mousedown", function(event) {
     let display = document.getElementById("display-container")   //you need to just iterate over the buttons
      let elementText = event.target.innerText
      
-    let displayValue = display.textContent + elementText
+    if (operator.value === "+") {
+        console.log("you did it")
+        display.textContent = "" + event.target.innerText      /// this works but its double firing the event because its listening to opeator as ell
+        let displayValue2 = "" + elementText
+        display.textContent = displayValue2
+        
+        } else {
+          let displayValue = display.textContent + elementText
+          display.textContent = displayValue
+
+        }
+
+
+
+
+
     
-    display.textContent = displayValue
     //console.log(displayValue)
 
     })
 
 // listens for num1
 
-buttons.addEventListener("click", function(event) {
+buttons.addEventListener("mousedown", function(event) {
     let display = document.getElementById("display-container")   // i have grabbed this value here. need to notremove from array or not listen to it intially
-        if (event.target.innerText == "+") {
-                let number = display.textContent.slice(0, -1)
-                num1.value = parseInt(number)
+        if (event.target.innerText == "+" && "value" in num1 != true) {
+                let slicedNumber = display.textContent.slice(0, -1)
+                num1.value = parseInt(slicedNumber)
                 console.log(num1.value)
+        }else {
+          console.log('reee')
         }
         
 }) 
@@ -115,15 +94,27 @@ buttons.addEventListener("mousedown", function(event){
         //console.log(operator)
     })
 
+
+
 // listen for num2
 
 buttons.addEventListener("mouseup", function(event) {
     let display = document.getElementById("display-container")
     let operatorButtons = document.getElementsByClassName("operator-button")       // START HERE: now thatthe event is registered. clear the display yext and listen for num2 untill another operator or equals sign is pressed
     for (let i = 0; i < operatorButtons.length; i++) {
-        if (event.target == operatorButtons[i]) {
-            console.log('ye')
-        }else {
+        if (event.target === operatorButtons[i] && "value" in num1 === true) {
+
+          let slicedNumber = display.textContent.slice(0, -1)
+          num2.value = parseInt(slicedNumber)
+
+            console.log(num2.value)
+            break
+        }else if(event.target != operatorButtons[i]) {
+            console.log('no')
+        }
+        
+        
+          else{
          
         
     }
@@ -132,4 +123,20 @@ buttons.addEventListener("mouseup", function(event) {
 
 
 
-// theabove function only takes a single inpout at a time. this needs to take an entire string look at previous projects. will be dom related
+//operate
+buttons.addEventListener("click", function(event) {
+  let display = document.getElementById("display-container")
+  let operatorButtons = document.getElementsByClassName("operator-button")       // bug: num2 is being reassigned in this part, listen for something else
+  for (let i = 0; i < operatorButtons.length; i++) {
+    if (num2.value === true) {
+    console.log("YEHHHHHHHHHHHHHH")
+  }else {
+    console.log("lmaooo got em")
+  }
+  }
+
+
+
+
+
+})
